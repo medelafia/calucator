c3e8a70293fb644513fd03585c2ea69dd029e54a
med <mohamedelafia732@gmail.com>
2023-08-01 12:58:49 +0100

    V-001

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-Object.prototype.sayHello = function(){
-    return "salut";
-}
+let operators = document.querySelectorAll(".operator"); 
+let numbers = document.querySelectorAll(".number"); 
+let display = document.querySelector(".display");
+let result = document.querySelector(".result");
+let clear = document.querySelector(".clear");
+let del = document.querySelector(".del");
+let operand = '';
+let nbr1 , nbr2; 
+
+numbers.forEach(number => {
+  number.addEventListener("click",() => {
+    display.innerHTML += number.innerHTML;
+  })
+
+});
+operators.forEach(oper => {
+    oper.addEventListener("click", () =>{
+        nbr1 = Number.parseFloat(display.innerHTML);
+        operand = oper.innerHTML;
+        display.innerHTML = "";
+        console.log(nbr1); 
+        console.log(operand);
+    })
+
+});
+result.addEventListener("click",()=> {
+    if(!nbr1 || display.innerHTML ==="" || operand ===''){
+        return false ; 
+    }else {
+        nbr2 = Number.parseFloat(display.innerHTML); 
+        switch(operand){
+            case "+" : display.innerHTML = nbr1 +nbr2 ;
+                       break; 
+            case "*" : display.innerHTML = nbr1 * nbr2 ;
+                       break; 
+            case "/" : display.innerHTML = nbr1 / nbr2 ;
+                       break; 
+            case "-" : display.innerHTML = nbr1 - nbr2 ;
+                       break;    
+            case "%" : display.innerHTML = nbr1 % nbr2 ;
+                       break;      
+        }
+    }
+});
+clear.addEventListener("click", () => {
+    display.innerHTML = ""; 
+    operand = ""; 
+    nbr1 = 0; 
+    nbr2 = 0; 
+}); 
+del.addEventListener("click",()=>{
+    display.innerHTML = display.innerHTML.slice(0,display.innerHTML.length -1);
+})
